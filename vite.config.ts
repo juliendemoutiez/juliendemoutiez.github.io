51d2bd9141028394bdcae358c8fc38a82bb7cd29
Julien Demoutiez <julien.demoutiez@gmail.com>
2025-01-29 11:58:30 +0100

    Add Github Pages deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/juliendemoutiez.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
